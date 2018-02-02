d59cc66e645b7787be7a258b7114b40494f9aa0d
skolis <swetha.a0427@gmail.com>
2018-02-02 06:40:55 -0800

    add practice folder

---
+++ pythonpractice/pythonformat.py
@@ -0,0 +1,9 @@
+import sys
+import datetime
+
+print("Twinkle, twinkle, little star,\n \tHow I wonder what you are! \n \t \tUp above the world so high,\n \t \tLike a diamond in the sky.\n Twinkle, twinkle, little star,\n \t How I wonder what you are")
+
+print ("python --version")
+print (sys.version)
+now = datetime.datetime.now()
+print ("date time is : " + now.strftime("%Y %m %d  %H:%M:%S"))
